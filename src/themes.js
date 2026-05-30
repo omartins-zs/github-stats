e7273c9d4ee78066eb756af3ab87fa1fd38cc909
omartins-zs <omartins-zs@users.noreply.github.com>
2026-05-30 12:06:47 -0300

    :sparkles: feat: criando renderizador de svg e temas

---
+++ src/themes.js
@@ -0,0 +1,44 @@
+export const themes = {
+  default: {
+    title_color: "2f80ed",
+    icon_color: "4c71f2",
+    text_color: "434d58",
+    bg_color: "fffefe",
+    border_color: "e4e2e2",
+  },
+  dark: {
+    title_color: "fff",
+    icon_color: "79ff97",
+    text_color: "9f9f9f",
+    bg_color: "151515",
+    border_color: "2a2a2a",
+  },
+  radical: {
+    title_color: "fe428e",
+    icon_color: "f8d847",
+    text_color: "a9fef7",
+    bg_color: "141321",
+    border_color: "2a2a2a",
+  },
+  dracula: {
+    title_color: "ff6e96",
+    icon_color: "79dafa",
+    text_color: "f8f8f2",
+    bg_color: "282a36",
+    border_color: "44475a",
+  },
+  prussian: {
+    title_color: "bddfff",
+    icon_color: "38a0ff",
+    text_color: "6e93b5",
+    bg_color: "172f45",
+    border_color: "234563",
+  },
+  nord: {
+    title_color: "81a1c1",
+    icon_color: "88c0d0",
+    text_color: "d8dee9",
+    bg_color: "2e3440",
+    border_color: "3b4252",
+  }
+};
